cee0b654d0e2beaa160cb6c03064bb409b7e1fa3
kidliaa <kidliaa@gmail.com>
2015-12-02 23:45:29 +0800

    init

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,7 +1,24 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 
+var unicodes = [
+   "↑",
+   "→",
+   "↓",
+   "←",
+   "┌",
+   "┐",
+]
+
+var list = unicodes.map(function(unicode, index){
+  return <li key={index} className="b w-100  pal pvxl pvl-ns mega  tc w-25-ns fl border-box">{unicode}</li>;
+})
 ReactDOM.render(
-  <p>Hello from ./src/js/main.jsx. Check this file out.</p>,
+  <div>
+    <p className="tc tl-ns pll-ns plxxl-l gray up"><a className="link ttu f3 blue-pure b" href="https://liaa.github.io/unicodo/">unicodo</a> some useful unicodes.</p>
+    <ul className="list pln mtl-ns cf">
+      {list}
+    </ul>
+  </div>,
   document.getElementById('app')
 );
