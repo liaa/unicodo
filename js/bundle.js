438a65ceb99e91d58b7da08a076a2f7e649a870e
kidliaa <kidliaa@gmail.com>
2015-12-03 08:25:37 +0800

    convenient subimit link

--- js/bundle.js
+++ js/bundle.js
@@ -64,7 +64,7 @@
 	    React.createElement("div", {className: "tc tl-ns pll-ns plxxl-l"}, 
 	    React.createElement("p", {className: "up"}, React.createElement("a", {className: "link ttu f3 blue-pure b", href: "https://liaa.github.io/unicodo/"}, "unicodo")), 
 	    React.createElement("p", {className: "man gray"}, "some useful unicodes"), 
-	    React.createElement("p", {className: "man"}, React.createElement("a", {href: "https://github.com/liaa/unicodo", target: "_blank", className: "link gray"}, "submit →"))
+	    React.createElement("p", {className: "man"}, React.createElement("a", {href: "https://github.com/liaa/unicodo/issues/new", target: "_blank", className: "link gray"}, "submit →"))
 	    ), 
 	    React.createElement("ul", {className: "list pln mtl-ns cf"}, 
 	      list
