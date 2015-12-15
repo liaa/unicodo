1366c2e0881113ea6b0d8166a4c6cb08cc31da75
kidliaa <kidliaa@gmail.com>
2015-12-15 10:55:07 +0800

    add unicode name

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -2,21 +2,51 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 
 var unicodes = [
-   "↑",
-   "→",
-   "↓",
-   "←",
-   "┌",
-   "┐",
+  "↑ Up arrow",
+  "→ Right arrow",
+  "↓ Down arrow",
+  "← Left arrow",
+  //"┌ ",
+  //"┐ ",
+  " Apple",
+  "⌘ Command",
+  "⌥ Option",
+  "⇪ Caps Lock",
+  "⇧ Shift",
+  "⌃ Control",
+  "␣ Space",
+  "⏎  Return",
+  "↩ Return",
+  "⎋ Escape",
+  "⇥ Tab forward",
+  "⇤  Tab back",
+  "⌽ Eject"
 ]
 
 var list = unicodes.map(function(unicode, index){
-  return <li key={index} className="b w-100  pal pvxl pvl-ns mega  tc w-25-ns fl border-box">{unicode}</li>;
+  var code = unicode.slice(0,1);
+  var name = unicode.slice(1);
+  return (
+    <li className="Unicode b w-100  pal pvxl  w-25-ns fl border-box" key={index}>
+      <div className="dt w-100 tc">
+        <div className="dtr mega ">
+          <div className="dtc">
+            {code}
+          </div>
+        </div>
+        <div className="dtr">
+          <div className="thin dtc ptm black-40">
+              {name}
+          </div>
+        </div>
+      </div>
+    </li>
+  )
 })
 ReactDOM.render(
-  <div>
+  <div className="">
     <div className="tc tl-ns pll-ns plxxl-l">
-    <p className="up"><a className="link ttu f3 blue-pure b" href="https://liaa.github.io/unicodo/">┌| unicodo |┐</a></p>
+    <p className="up"><a className="link ttu f3 blue-pure b" href="https://liaa.github.io/unicodo/">unicodo</a></p>
     <p className="man gray">some useful unicodes</p>
     <p className="man"><a href="https://github.com/liaa/unicodo" target="_blank" className="link gray">submit →</a></p>
     </div>
